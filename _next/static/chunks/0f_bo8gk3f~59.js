d5ec3517c37d055e1ca7395410209e2d68ac8858
AviweMbolekwa <mbolekwaaviwe@gmail.com>
2026-09-23 23:49:54 +0200

    Deploy static site from 415bc61

---
+++ _next/static/chunks/0f_bo8gk3f~59.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,1992,t=>{t.v(o=>Promise.all(["static/chunks/0bmrsu_9~2xd8.js"].map(o=>t.l(o))).then(()=>o(23227)))}]);
